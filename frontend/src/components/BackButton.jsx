02a179920338d2da86797efb196635473b7d8d6f
Jay <76460079+PZDonny@users.noreply.github.com>
2026-04-13 01:29:02 -0500

    add backbutton component

---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import '../styles/BackButton.css'
+
+function BackButton({ text, to }) {
+  return (
+    <div className="back-btn">
+      <Link to={to}>{`← ${text}`}</Link>
+    </div>
+  );
+}
+
+export default BackButton;
