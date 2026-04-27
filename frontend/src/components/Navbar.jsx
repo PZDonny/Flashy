5480544bc4130e1d3d67ebfadf742bdc773e5cd2
Jay <76460079+PZDonny@users.noreply.github.com>
2026-04-26 22:28:32 -0500

    make navbar "sticky"

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,43 +1,45 @@
-import { Link } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
-import { api } from "../api.js";
-import "../styles/Navbar.css";
+import { Link } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+import { api } from '../api.js';
+import '../styles/Navbar.css';
 
 export default function Navbar() {
   const { user } = useAuth();
 
   return (
-    <nav className="navbar">
-      <div className="navbar-flashy">
-        <Link to={user ? "/dashboard" : "/"}>Flashy</Link>
-      </div>
+    <div className='navbar-container'>
+      <nav className='navbar'>
+        <div className='flashy'>
+          <Link to={user ? '/dashboard' : '/'}>Flashy</Link>
+        </div>
 
-      <div className="navbar-links">
-        {user ? (
-          <>
-            <Link to="/dashboard" className="set-btn">
-              My Sets
-            </Link>
-            <div className="user-section">
-              <span className="username">{user.username}</span>
-              <Link to="/">
-                <button className="logout-btn" onClick={api.logout}>
-                  Logout
-                </button>
+        <div className='links'>
+          {user ? (
+            <>
+              <Link to='/dashboard' className='set-btn'>
+                My Sets
               </Link>
-            </div>
-          </>
-        ) : (
-          <>
-            <Link to="/login" className="nav-btn">
-              Login
-            </Link>
-            <Link to="/register" className="nav-btn">
-              Register
-            </Link>
-          </>
-        )}
-      </div>
-    </nav>
+              <div className='user-section'>
+                <span className='username'>{user.username}</span>
+                <Link to='/'>
+                  <button className='logout-btn' onClick={api.logout}>
+                    Logout
+                  </button>
+                </Link>
+              </div>
+            </>
+          ) : (
+            <>
+              <Link to='/login' className='nav-btn'>
+                Login
+              </Link>
+              <Link to='/register' className='nav-btn'>
+                Register
+              </Link>
+            </>
+          )}
+        </div>
+      </nav>
+    </div>
   );
 }
